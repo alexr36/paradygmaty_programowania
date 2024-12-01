2f7ae5c0a0ce308f6d6ebf253ee65de05947bc44
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2024-12-01 21:45:28 +0100

    Update List2.ml
    Added descriptions to some functions

--- Ćwiczenia/OCAML/List2.ml
+++ Ćwiczenia/OCAML/List2.ml
@@ -4,22 +4,22 @@
 
 (* Wersja "z definicji" *)
 let rec fib n =
-  match n with
-  | n when (n < 0) -> raise (Failure "Nie mozna obliczyc wyrazu ciagu o pozycji mniejszej niz zero.")
-  | 0 -> 0
-  | 1 -> 1
-  | n -> fib (n - 1) + fib (n - 2)
+  match n with                                                                                                                    (* Dopasowanie wartości n *)
+  | n when (n < 0) -> raise (Failure "Nie mozna obliczyc wyrazu ciagu o pozycji mniejszej niz zero.")                             (* Jeśli mniejsze niż 0, rzuć wyjątek *)
+  | 0 -> 0                                                                                                                        (* Jeśli równe 0, zwróć 0 *)
+  | 1 -> 1                                                                                                                        (* Jeśli równe 1, zwróć 1 *)
+  | n -> fib (n - 1) + fib (n - 2)                                                                                                (* Jeśli większe od 1, oblicz kolejny (poprzedni) wyraz ciągu *)
 
 
 (* Wersja z rekurencją ogonową *)
 let fibTail n =
-  let rec helperFunc n previousTerm currentTerm =
-    match n with
-    | n when (n < 0) -> raise (Failure "Nie mozna obliczyc wyrazu ciagu o pozycji mniejszej niz zero.")
-    | 0 -> previousTerm
-    | n -> helperFunc (n - 1) currentTerm (currentTerm + previousTerm)
+  let rec helperFunc n previousTerm currentTerm =                                                                                 (* Funkcja pomocnicza *)
+    match n with                                                                                                                  (* Dopasowanie wartości n *)
+    | n when (n < 0) -> raise (Failure "Nie mozna obliczyc wyrazu ciagu o pozycji mniejszej niz zero.")                           (* Jeśli mniejsze niż 0, rzuć wyjątek *)
+    | 0 -> previousTerm                                                                                                           (* Jeśli równe 0, zwróć poprzedni wyraz ciągu *)
+    | n -> helperFunc (n - 1) currentTerm (currentTerm + previousTerm)                                                            (* Jeśli większe od 0, wywołaj funkcję pomocniczą i oblicz następny wyraz ciągu; zastąp poprzedni wyraz aktualnym wyrazem *)
   in
-  helperFunc n 0 1
+  helperFunc n 0 1                                                                                                                (* Wywołaj funkcję wewnętrzną dla wartości n, poprzedniego wyrazu = 0 i aktualnego wyrazu = 1 *)
 
 (* TESTY 
 
@@ -78,10 +78,10 @@ matchCaseTwo [(1, 3); (4, 0)];;
 (* -- ZADANIE 5   ---------------------------------------------------------------------------------------------------------- *)
 
 let rec initSegment (xs, ys) =
-  match (xs, ys) with
-  | ([], _) -> true
-  | (_, []) -> false
-  | (xs_head :: xs_tail, ys_head :: ys_tail) -> xs_head = ys_head && initSegment (xs_tail, ys_tail)
+  match (xs, ys) with                                                                                                          (* Dopasowanie wartości list xs i ys *)
+  | ([], _) -> true                                                                                                            (* Jeśli xs jest puste, zwróć true *)
+  | (_, []) -> false                                                                                                           (* Jeśli ys jest puste, zwróć false *)
+  | (xs_head :: xs_tail, ys_head :: ys_tail) -> xs_head = ys_head && initSegment (xs_tail, ys_tail)                            (* Jeśli obie listy mają elementy, weź głowy list i porównaj je, wywołaj funkcję dla ogonów tych list *)
 
 
 (* TESTY
@@ -94,10 +94,10 @@ initSegment ([1; 3], [1; 2; 3; 4]);;
 (* -- ZADANIE 6   ---------------------------------------------------------------------------------------------------------- *)
 
 let rec replaceNth (list, n, a) =
-  match list with
-  | [] -> []
-  | _ :: listTail when (n = 0) -> a :: listTail
-  | listHead :: listTail -> listHead :: replaceNth (listTail, n - 1, a)
+  match list with                                                                                                              (* Dopasowanie wartości listy *)
+  | [] -> []                                                                                                                   (* Jeśli jest pusta, zwróć listę pustą *)
+  | _ :: listTail when (n = 0) -> a :: listTail                                                                                (* Jeśli ma więcej niż 1 element a licznik n = 0, dodaj element a do ogona listy, pomijając jej głowę (zastępując) *)
+  | listHead :: listTail -> listHead :: replaceNth (listTail, n - 1, a)                                                        (* Jeśli licznik n jest > 0, weź głowę aktualnej listy i dodaj ją do listy wynikowej, otrzymanej z wywołania funkcji dla zmniejszonego licznika n *)
 
 (* TESTY
 
