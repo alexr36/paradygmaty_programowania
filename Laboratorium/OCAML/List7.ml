bac6aea5ebc26eac501108b953ba9abeba3b0ce2
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2024-12-13 21:26:34 +0100

    Update List7.ml

--- Laboratorium/OCAML/List7.ml
+++ Laboratorium/OCAML/List7.ml
@@ -14,15 +14,14 @@ type 'a drzewo =
 
 
 let rec isSubtree tree1 tree2 =
-  match (tree1, tree2) with
-  | (Lisc leaf1, Lisc leaf2) -> leaf1 = leaf2
-  | (Wezel (left1, right1), Wezel (left2, right2)) ->
-    (isSubtree left1 tree2) || (isSubtree left2 tree2) ||
-    (isSubtree left1 left2 && isSubtree right1 right2)
-  | (_, Wezel (left, right)) ->
-    isSubtree tree1 left || isSubtree tree1 right
-  | _ -> false
-
+  match (tree1, tree2) with                                                                   (* Dopasuj parę drzew do wzorca *)
+  | (Lisc leaf1, Lisc leaf2) -> leaf1 = leaf2                                                 (* Jeśli obydwa drzewa to liście, przyrownaj je *)
+  | (Wezel (left1, right1), Wezel (left2, right2)) ->                                         (* Jeśli obydwa drzewa to węzły, *)
+    (isSubtree left1 tree2) || (isSubtree left2 tree2) ||                                     (* Wywołaj funkcję dla pierwszego lewego poddrzewa, drugiego lewego poddrzewa *)
+    (isSubtree left1 left2 && isSubtree right1 right2)                                        (* oraz dla wszystkich poddrzew *)
+  | (_, Wezel (left, right)) ->                                                               (* Jeśli drzewo, do ktorego przyrownujemy jest węzłem, *)
+    isSubtree tree1 left || isSubtree tree1 right                                             (* Wywołaj funkcję dla jego lewego i prawego poddrzewa *)
+  | _ -> false                                                                                (* W kazdym innym przypadku, zwroc fałsz *)
 
 
   let tree1 = Wezel (Lisc 1, Lisc 2)
@@ -33,9 +32,9 @@ let rec isSubtree tree1 tree2 =
   
   (*
   
-  isSubtree tree1 tree3;;
-  isSubtree tree2 tree3;;
-  isSubtree tree4 tree3;;
-  isSubtree tree5 tree3;;
+    isSubtree tree1 tree3;;
+    isSubtree tree2 tree3;;
+    isSubtree tree4 tree3;;
+    isSubtree tree5 tree3;;
   
   *)     
