00268a9d7c9ffc5c1a78b21f7868d513378bdb23
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-01-14 14:37:44 +0100

    Update Main.scala

--- Ćwiczenia/SCALA/List10/Task3/Main.scala
+++ Ćwiczenia/SCALA/List10/Task3/Main.scala
@@ -5,7 +5,8 @@ class Main
 object Main extends App {
   val queue = MyQueue(4, 6, 7, 1, 1, 15)
 
-  println(queue)
-  println(queue.dequeue)
-  println(queue.enqueue(57))
+  println(queue)                //  4, 6, 7, 1, 1, 15
+  println(queue.dequeue)        //  6, 7, 1, 1, 15
+  println(queue.enqueue(57))    //  4, 6, 7, 1, 1, 15, 57
+  println(queue.first)          //  4
 }
