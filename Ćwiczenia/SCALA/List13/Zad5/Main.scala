46c73f1cdb7f30e06f17dea61270447385b89c20
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-02-10 17:17:57 +0100

    Add files via upload

--- Ćwiczenia/SCALA/List13/Zad5/Main.scala
+++ Ćwiczenia/SCALA/List13/Zad5/Main.scala
@@ -1,3 +1,16 @@
 package Zad5
 
 class Main
+
+
+object Main extends App {
+  //  Hamlet by William Skaespeare - Act 1, Scene 1 - Synopsis
+  private val text =
+    "Synopsis:\n\nOn the guards’ platform at Elsinore, " +
+    "Horatio waits with Barnardo and Marcellus to question a ghost that has twice before appeared. " +
+    "The Ghost, in the form of the late King Hamlet of Denmark, appears but will not speak. " +
+    "Horatio decides to tell his fellow student, Prince Hamlet, about the Ghost’s appearance."
+  private val word_counter = new WordCounter()
+
+  println(word_counter.wordCounter(text))
+}
--- Ćwiczenia/SCALA/List13/Zad5/WordCounter.scala
+++ Ćwiczenia/SCALA/List13/Zad5/WordCounter.scala
@@ -1,7 +1,17 @@
 package Zad5
 
+
 class WordCounter {
-  def wordCounter(text: String): Map[String, Int] = {
-    
+  def wordCounter(text: String): scala.collection.mutable.Map[String, Int] = {
+    val result = scala.collection.mutable.Map[String, Int]()
+    val text_to_words = text.split(" ")
+
+    text_to_words.foreach(word => {
+      if (word.nonEmpty) {    //  Omits empty words
+        result(word) = result.getOrElse(word, 0) + 1
+      }
+    })
+
+    result
   }
 }
