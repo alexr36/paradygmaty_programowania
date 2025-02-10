8f66aa375216b899e41088623437900d0893f196
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-02-10 10:20:50 +0100

    Update Main.scala

--- Ćwiczenia/SCALA/List13/Zad4/Main.scala
+++ Ćwiczenia/SCALA/List13/Zad4/Main.scala
@@ -7,11 +7,6 @@ object Main extends App {
   val cylinder = new Cylinder()
 
   println(cylinder)
-
-  cylinder.setX(5.2)
-  cylinder.setY(6.0)
-  cylinder.setRadius(7.21)
-  cylinder.setHeight(0.98)
-
+  cylinder.setX(5.2).setY(6.0).setRadius(7.21).setHeight(0.98)
   print(cylinder)
 }
